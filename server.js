beb061226b6194e3d2b3f503e8353936ded313c6
oscarmx <omejia94@gmail.com>
2019-09-30 21:11:53 -0500

    clase18

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
-const app = express();
-const port = 9000;
+const app = express()
+const port = 9000
 
-app.use(express.static('public'));
 
-app.listen(port, () => {
-    console.log("Servidor escuchando en el puerto " + port);  
+const router = express.Router()
+
+/*router.get('/',(req,res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
+app.use('/asset', router)
+app.listen(9000, () => { 
+    console.log(`Example app listening on port ${port}!`)
+})*/
+
+router.get('/',(req,res) => {
+    res.sendFile(__dirname + '/index2.html')
+})
+
+app.use('/', router)
+
+app.listen(9000, () => { 
+    console.log(`Example app listening on port ${port}!`)
 })
